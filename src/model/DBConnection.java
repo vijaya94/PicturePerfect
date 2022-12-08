eee968e65e2623a53cf6da80b884943cdc36e8d4
Harsh Patel <113477769+Deku-21@users.noreply.github.com>
2022-12-08 10:32:41 -0500

    Customer part changes

---
+++ src/model/DBConnection.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Vijaya B.
+ */
+public class DBConnection {
+    public static Connection con(){
+        Connection con = null;
+        try{
+          //  Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pictureperfect","root","");
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null, e);
+        }
+        return con;
+    }
+}
